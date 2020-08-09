5b6feb5daa34fe9b5b14525032f9a96fda35505b
Yooncoding <63987872+Yooncoding@users.noreply.github.com>
2020-08-09 17:15:54 +0900

    Scroll Animaition - section_first 1

--- JS/main.js
+++ JS/main.js
@@ -5,67 +5,203 @@
   let prevScrollHeight = 0;
   // 현재 보여지는 scoll_section
   let currentScene = 0;
-  // scene 바뀌는 곳 체크 
+  // scene 바뀌는 곳 체크
   let enterNewScene = false;
   const sceneInfo = [{
       // scroll_section-first
-      type: 'sticky',
+      type: "sticky",
       scrollHeight: 0,
       heightNum: 5,
       objs: {
-        container: document.querySelector('#scroll_section-first'),
-        mentionFirst: document.querySelector('#scroll_section-first .main_mention.first'),
-        mentionSecond: document.querySelector('#scroll_section-first .main_mention.second'),
-        mentionThird: document.querySelector('#scroll_section-first .main_mention.third'),
-        mentionForth: document.querySelector('#scroll_section-first .main_mention.forth')
+        container: document.querySelector("#scroll_section-first"),
+        mentionFirst: document.querySelector(
+          "#scroll_section-first .main_mention.first"
+        ),
+        mentionSecond: document.querySelector(
+          "#scroll_section-first .main_mention.second"
+        ),
+        mentionThird: document.querySelector(
+          "#scroll_section-first .main_mention.third"
+        ),
+        mentionForth: document.querySelector(
+          "#scroll_section-first .main_mention.forth"
+        ),
       },
       values: {
-        mentionFirst_opacity: [0, 1]
-      }
+        // in
+        mentionFirst_opacity_in: [
+          0,
+          1,
+          {
+            start: 0.1,
+            end: 0.2
+          },
+        ],
+        mentionFirst_translateY_in: [
+          20,
+          0,
+          {
+            start: 0.1,
+            end: 0.2
+          },
+        ],
+        mentionSecond_opacity_in: [
+          0,
+          1, {
+            start: 0.3,
+            end: 0.4
+          },
+        ],
+        mentionSecond_translateY_in: [
+          20,
+          0,
+          {
+            start: 0.3,
+            end: 0.4
+          },
+        ],
+        mentionThird_opacity_in: [
+          0,
+          1, {
+            start: 0.5,
+            end: 0.6
+          },
+        ],
+        mentionThird_translateY_in: [
+          20,
+          0,
+          {
+            start: 0.5,
+            end: 0.6
+          },
+        ],
+        mentionForth_opacity_in: [
+          0,
+          1, {
+            start: 0.7,
+            end: 0.8
+          },
+        ],
+        mentionForth_translateY_in: [
+          20,
+          0,
+          {
+            start: 0.7,
+            end: 0.8
+          },
+        ],
+        // out
+        mentionFirst_opacity_out: [
+          1,
+          0,
+          {
+            start: 0.25,
+            end: 0.3
+          },
+        ],
+        mentionFirst_translateY_out: [
+          0,
+          -20,
+          {
+            start: 0.25,
+            end: 0.3
+          },
+        ],
+        mentionSecond_opacity_out: [
+          1,
+          0,
+          {
+            start: 0.45,
+            end: 0.5
+          },
+        ],
+        mentionSecond_translateY_out: [
+          0,
+          -20,
+          {
+            start: 0.45,
+            end: 0.5
+          },
+        ],
+        mentionThird_opacity_out: [
+          1,
+          0,
+          {
+            start: 0.65,
+            end: 0.7
+          },
+        ],
+        mentionThird_translateY_out: [
+          0,
+          -20,
+          {
+            start: 0.65,
+            end: 0.7
+          },
+        ],
+        mentionForth_opacity_out: [
+          1,
+          0,
+          {
+            start: 0.85,
+            end: 0.9
+          },
+        ],
+        mentionForth_translateY_out: [
+          0,
+          -20,
+          {
+            start: 0.85,
+            end: 0.9
+          },
+        ],
+      },
     },
     {
       // scroll_section-second
-      type: 'normal',
+      type: "normal",
       scrollHeight: 0,
       heightNum: 5,
       objs: {
-        container: document.querySelector('#scroll_section-second')
-      }
+        container: document.querySelector("#scroll_section-second"),
+      },
     },
     {
       // scroll_section-third
-      type: 'sticky',
+      type: "sticky",
       scrollHeight: 0,
       heightNum: 5,
       objs: {
-        container: document.querySelector('#scroll_section-third')
-      }
+        container: document.querySelector("#scroll_section-third"),
+      },
     },
     {
       // scroll_section-forth
-      type: 'sticky',
+      type: "sticky",
       scrollHeight: 0,
       heightNum: 5,
       objs: {
-        container: document.querySelector('#scroll_section-forth')
-      }
+        container: document.querySelector("#scroll_section-forth"),
+      },
     },
     {
       // scroll_section-fifth
-      type: 'normal',
+      type: "normal",
       scrollHeight: 0,
       heightNum: 5,
       objs: {
-        container: document.querySelector('#scroll_section-fifth')
-      }
-    }
+        container: document.querySelector("#scroll_section-fifth"),
+      },
+    },
   ];
 
   function setSectionHeight() {
     // 각 scroll_section에 높이 셋팅
     for (let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
-      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+      sceneInfo[
+        i
+      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
     // load나 resize될 때 현재 scene 반영하기
     // totalScrollHeight은 새로고침 될때 현재 있는 0~현재있는 페이지의 높이만큼 다 더한값이다.
@@ -78,7 +214,7 @@
         break;
       }
     }
-    document.body.setAttribute('id', `show_scene-${currentScene}`);
+    document.body.setAttribute("id", `show_scene-${currentScene}`);
   }
 
   function scrollLoop() {
@@ -87,19 +223,20 @@
     enterNewScene = false;
     // for문은 i가 1 이상일때 부터 시행
     for (let i = 0; i < currentScene; i++) {
-      prevScrollHeight = prevScrollHeight + sceneInfo[currentScene].scrollHeight;
+      prevScrollHeight =
+        prevScrollHeight + sceneInfo[currentScene].scrollHeight;
     }
     // c가 0일때 prevScrollHeight는 0,sceneInfo[currentScene].scrollHeight)는 4090이다.
     if (yOffSet > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       enterNewScene = true;
       currentScene++;
-      document.body.setAttribute('id', `show_scene-${currentScene}`);
+      document.body.setAttribute("id", `show_scene-${currentScene}`);
     }
     if (yOffSet < prevScrollHeight) {
       if (yOffSet == 0) return;
       enterNewScene = true;
       currentScene--;
-      document.body.setAttribute('id', `show_scene-${currentScene}`);
+      document.body.setAttribute("id", `show_scene-${currentScene}`);
     }
     // scene 바뀌는 곳에서 한번 scrollLoop 탈출 -> 일시적으로 바뀌는부분에서 -가 나오므로 그떼 playAnimaitionn 실행 않도록
     if (enterNewScene) return;
@@ -108,20 +245,123 @@
 
   // 여기 들어가는 변수는 values 오브젝트
   function clacValuese(values, currentYOffset) {
-    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
-    return scrollRatio * (values[1] - values[0]) + values[0];
+    let returnValues = 0;
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
+    if (values.length === 3) {
+      // values.length가 3이라는것은 start와 end, 구간이 정해져있다는 것
+      const partScrollStart = values[2].start * scrollHeight;
+      const partScrollEnd = values[2].end * scrollHeight;
+      const partScrollHeight = partScrollEnd - partScrollStart;
+      if (
+        currentYOffset >= partScrollStart &&
+        currentYOffset <= partScrollEnd
+      ) {
+        returnValues =
+          ((currentYOffset - partScrollStart) / partScrollHeight) *
+          (values[1] - values[0]) +
+          values[0];
+      } else if (currentYOffset < partScrollStart) {
+        returnValues = values[0];
+      } else if (currentYOffset > partScrollEnd) {
+        returnValues = values[1];
+      }
+    } else {
+      // else일때는 section전체가 구간이라는 것
+      returnValues = scrollRatio * (values[1] - values[0]) + values[0];
+    }
+    return returnValues;
   }
 
   function playAnimation() {
     const objs = sceneInfo[currentScene].objs;
     const values = sceneInfo[currentScene].values;
     const currentYOffset = yOffSet - prevScrollHeight;
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
     // console.log(currentScene, currentYOffset);
     switch (currentScene) {
       case 0:
-        let mentionFirst_opacity_in = clacValuese(values.mentionFirst_opacity, currentYOffset);
-        objs.mentionFirst.style.opacity = mentionFirst_opacity_in;
-        console.log((clacValuese(values.mentionFirst_opacity, currentYOffset)));
+        // mentionFirst 세팅
+        let mentionFirst_opacity_in = clacValuese(
+          values.mentionFirst_opacity_in, currentYOffset
+        );
+        let mentionFirst_opacity_out = clacValuese(
+          values.mentionFirst_opacity_out, currentYOffset
+        );
+        let mentionFirst_translateY_in = clacValuese(
+          values.mentionFirst_translateY_in, currentYOffset
+        );
+        let mentionFirst_translateY_out = clacValuese(
+          values.mentionFirst_translateY_out, currentYOffset
+        );
+        if (scrollRatio <= 0.22) {
+          objs.mentionFirst.style.opacity = mentionFirst_opacity_in;
+          objs.mentionFirst.style.transform = `translateY(${mentionFirst_translateY_in}%)`;
+        } else if (scrollRatio > 0.22) {
+          objs.mentionFirst.style.opacity = mentionFirst_opacity_out;
+          objs.mentionFirst.style.transform = `translateY(${mentionFirst_translateY_out}%)`;
+        }
+        // mentionSecond 세팅
+        let mentionSecond_opacity_in = clacValuese(
+          values.mentionSecond_opacity_in, currentYOffset
+        );
+        let mentionSecond_opacity_out = clacValuese(
+          values.mentionSecond_opacity_out, currentYOffset
+        );
+        let mentionSecond_translateY_in = clacValuese(
+          values.mentionSecond_translateY_in, currentYOffset
+        );
+        let mentionSecond_translateY_out = clacValuese(
+          values.mentionSecond_translateY_out, currentYOffset
+        );
+        if (scrollRatio <= 0.42) {
+          objs.mentionSecond.style.opacity = mentionSecond_opacity_in;
+          objs.mentionSecond.style.transform = `translateY(${mentionSecond_translateY_in}%)`;
+        } else if (scrollRatio > 0.42) {
+          objs.mentionSecond.style.opacity = mentionSecond_opacity_out;
+          objs.mentionSecond.style.transform = `translateY(${mentionSecond_translateY_out}%)`;
+        }
+        // mentionThird 세팅
+        let mentionThird_opacity_in = clacValuese(
+          values.mentionThird_opacity_in, currentYOffset
+        );
+        let mentionThird_opacity_out = clacValuese(
+          values.mentionThird_opacity_out, currentYOffset
+        );
+        let mentionThird_translateY_in = clacValuese(
+          values.mentionThird_translateY_in, currentYOffset
+        );
+        let mentionThird_translateY_out = clacValuese(
+          values.mentionThird_translateY_out, currentYOffset
+        );
+        if (scrollRatio <= 0.62) {
+          objs.mentionThird.style.opacity = mentionThird_opacity_in;
+          objs.mentionThird.style.transform = `translateY(${mentionThird_translateY_in}%)`;
+        } else if (scrollRatio > 0.62) {
+          objs.mentionThird.style.opacity = mentionThird_opacity_out;
+          objs.mentionThird.style.transform = `translateY(${mentionThird_translateY_out}%)`;
+        }
+        // mentionForth 세팅
+        let mentionForth_opacity_in = clacValuese(
+          values.mentionForth_opacity_in, currentYOffset
+        );
+        let mentionForth_opacity_out = clacValuese(
+          values.mentionForth_opacity_out, currentYOffset
+        );
+        let mentionForth_translateY_in = clacValuese(
+          values.mentionForth_translateY_in, currentYOffset
+        );
+        let mentionForth_translateY_out = clacValuese(
+          values.mentionForth_translateY_out, currentYOffset
+        );
+        if (scrollRatio <= 0.82) {
+          objs.mentionForth.style.opacity = mentionForth_opacity_in;
+          objs.mentionForth.style.transform = `translateY(${mentionForth_translateY_in}%)`;
+        } else if (scrollRatio > 0.82) {
+          objs.mentionForth.style.opacity = mentionForth_opacity_out;
+          objs.mentionForth.style.transform = `translateY(${mentionForth_translateY_out}%)`;
+        }
         break;
       case 1:
         break;
@@ -132,14 +372,13 @@
       case 4:
         break;
     }
-
   }
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     yOffSet = window.pageYOffset;
     scrollLoop();
   });
-  window.addEventListener('resize', setSectionHeight);
-  window.addEventListener('load', setSectionHeight);
+  window.addEventListener("resize", setSectionHeight);
+  window.addEventListener("load", setSectionHeight);
 
   setSectionHeight();
 })();
